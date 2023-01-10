c206b44d5d8ae1fe710cb7d833ba5edf73d86553
Ali Nasrollahy <a.nasrollahi@arad.co.net>
2023-01-10 10:57:13 +0330

    cleaning main.cpp file

--- histogram_diagram/src/sources/main.cpp
+++ histogram_diagram/src/sources/main.cpp
@@ -1,22 +1,10 @@
 #include "headers/mainwindow.h"
 
 #include <QApplication>
-#include <QLocale>
-#include <QTranslator>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-
-    QTranslator translator;
-    const QStringList uiLanguages = QLocale::system().uiLanguages();
-    for (const QString &locale : uiLanguages) {
-        const QString baseName = "histogram_diagram_" + QLocale(locale).name();
-        if (translator.load(":/i18n/" + baseName)) {
-            a.installTranslator(&translator);
-            break;
-        }
-    }
 
     Arad::MainWindow w;
     w.show();
